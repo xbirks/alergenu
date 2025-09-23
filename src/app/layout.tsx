739d53700aa5c4f7312ca5a5112a91c77f3c0698
Andrés Ortega <andres03ortega@gmail.com>
2025-09-23 12:52:37 +0000

    Pon el fondo blanco que gris me parece una app básica de Android. Y camb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
       </head>
       <body className="antialiased">
         {children}
