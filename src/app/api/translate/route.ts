8c14f95be42b0e3269b84a45234c7e9e3b74fda8
Andrés Ortega <andres03ortega@gmail.com>
2025-10-16 11:34:51 +0000

    intento de cambios para desplieuge v3

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
   }
 
   const { text, targetLang } = requestBody;
-  const apiKey = process.env.TRADUCTOR_API;
+  const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY;
 
   if (apiKey) {
     console.log('Google Translate API Key found. Starts with:', apiKey.substring(0, 5));
